e1ebea4c1b6fdf88f5808c135712586beda0bbdb
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-03-15 12:26:31 +0330

    asdf

--- Arduino/03 24 Esfand (Farhan)/main/Strategy.ino
+++ Arduino/03 24 Esfand (Farhan)/main/Strategy.ino
@@ -1,17 +1,26 @@
 int cnt = 0;
 int out_cnt = 0;
+int cnt_ball_in_kick = 0;
+
 void Forward() {
   out();
   if (digitalRead(PA11)) {
     motor(0, 0, 0, 0);
   } else if (ball_in_kicker || ((ball_ang < 10 || ball_ang > 350) && ball_dist < 60 && is_ball)) {
+    cnt_ball_in_kick++;
     V = 600;
-    moveAngle(0);
-    shoot();
+    if (cnt_ball_in_kick > 20) {
+      moveAngle(180);
+      if (cnt_ball_in_kick > 40) cnt_ball_in_kick = 0;
+    } else {
+      moveAngle(0);
+      shoot();
+    }
   } else if (is_ball) {
+    cnt_ball_in_kick = 0;
     cnt = 0;
-    if (robot_id == 1) V = (ball_dist - 30) * 8;
-    else V = (ball_dist - 30) * 5;
+    if (robot_id == 1) V = (ball_dist - 30) * 6;
+    else V = (ball_dist - 30) * 6;
     if (ball_dist < 100) {
       if (ball_ang < 25 || ball_ang > 335) {
         V = 600;
@@ -28,11 +37,20 @@ void Forward() {
     }
   } else {
     cnt++;
-    if (cnt < 18) {
-      V = 200;
-      moveAngle(180);
-    } else motor(zavie_robot, zavie_robot, zavie_robot, zavie_robot);
+    comeback_center();
+    // comeback_simple();
   }
+}
+void comeback_simple() {
+  if (cnt < 22) {
+    V = 300;
+    moveAngle(180);
+  } else motor(zavie_robot, zavie_robot, zavie_robot, zavie_robot);
+}
+void comeback_center() {
+  V = 200;
+  if (distance_b > 150) motor(-V - diff + zavie_robot, -V + diff + zavie_robot, V + diff + zavie_robot, V - diff + zavie_robot);
+  else motor(zavie_robot, zavie_robot, zavie_robot, zavie_robot);
 }
 void reset_ldr_bools() {
   ldr_front = false;
@@ -51,7 +69,7 @@ void out() {
     while (ball_ang > 180 && is_ball && out_cnt < OUT_TIMEOUT) {
       TDread_noSkip();
       read_cam(false);
-      read_ldr();
+      read_sensors();
       if (digitalRead(PA11)) print_adc();
       else print_all();
       motor(zavie_robot, zavie_robot, zavie_robot, zavie_robot);
@@ -66,7 +84,7 @@ void out() {
     while (ball_ang < 180 && is_ball && out_cnt < OUT_TIMEOUT) {
       TDread_noSkip();
       read_cam(false);
-      read_ldr();
+      read_sensors();
       if (digitalRead(PA11)) print_adc();
       else print_all();
       motor(zavie_robot, zavie_robot, zavie_robot, zavie_robot);
@@ -78,10 +96,10 @@ void out() {
     move4sec(180, MOVE_SEC);
     out_cnt = 0;
     reset_ldr_bools();
-    while ((ball_ang < 90 || ball_ang > 270 )&& is_ball && out_cnt < OUT_TIMEOUT) {
+    while ((ball_ang < 90 || ball_ang > 270) && is_ball && out_cnt < OUT_TIMEOUT) {
       TDread_noSkip();
       read_cam(false);
-      read_ldr();
+      read_sensors();
       if (digitalRead(PA11)) print_adc();
       else print_all();
       motor(zavie_robot, zavie_robot, zavie_robot, zavie_robot);
@@ -90,13 +108,13 @@ void out() {
     reset_ldr_bools();
   }
   if (ldr_back) {
-    move4sec(0 , MOVE_SEC);
+    move4sec(0, MOVE_SEC);
     out_cnt = 0;
     reset_ldr_bools();
     while (ball_ang > 90 && ball_ang < 2700 && is_ball && out_cnt < OUT_TIMEOUT) {
       TDread_noSkip();
       read_cam(false);
-      read_ldr();
+      read_sensors();
       if (digitalRead(PA11)) print_adc();
       else print_all();
       motor(zavie_robot, zavie_robot, zavie_robot, zavie_robot);
